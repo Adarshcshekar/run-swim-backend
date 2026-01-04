6d85409068cef7cdc7da69fa7cc74defa1db050b
Adarsh C <AdarshC@Adarshs-MacBook.local>
2026-01-04 14:14:05 +0530

    added date filter

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -14,7 +14,20 @@ router.post("/run", auth, async (req, res) => {
 
 /* GET RUNS */
 router.get("/runs", auth, async (req, res) => {
-  const runs = await Run.find({ userId: req.userId });
+  const { date } = req.query; // optional
+  const filter = { userId: req.userId };
+
+  if (date) {
+    const start = new Date(date);
+    start.setHours(0, 0, 0, 0);
+
+    const end = new Date(date);
+    end.setHours(23, 59, 59, 999);
+
+    filter.date = { $gte: start, $lte: end };
+  }
+
+  const runs = await Run.find(filter).sort({ date: -1 });
   res.json(runs);
 });
 
@@ -27,7 +40,20 @@ router.post("/swim", auth, async (req, res) => {
 
 /* GET SWIMS */
 router.get("/swims", auth, async (req, res) => {
-  const swims = await Swim.find({ userId: req.userId });
+  const { date } = req.query; // optional
+  const filter = { userId: req.userId };
+
+  if (date) {
+    const start = new Date(date);
+    start.setHours(0, 0, 0, 0);
+
+    const end = new Date(date);
+    end.setHours(23, 59, 59, 999);
+
+    filter.date = { $gte: start, $lte: end };
+  }
+
+  const swims = await Swim.find(filter).sort({ date: -1 });
   res.json(swims);
 });
 
